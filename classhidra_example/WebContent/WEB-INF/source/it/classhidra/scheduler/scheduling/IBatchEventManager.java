61a5a14f5ec5c8b29bb4cc15abf8da51fd0dbb35
svyatoslav urbanovych <svyatoslav.urbanovych@gmail.com>
2022-04-18 21:12:45 +0200

    fix prev commit

---
+++ classhidra_example/WebContent/WEB-INF/source/it/classhidra/scheduler/scheduling/IBatchEventManager.java
@@ -0,0 +1,17 @@
+package it.classhidra.scheduler.scheduling;
+
+import it.classhidra.scheduler.scheduling.db.db_batch;
+import it.classhidra.scheduler.scheduling.db.i_batch_log;
+
+public interface IBatchEventManager {
+    public final static short EVENT_STILL_INEXEC = 1;
+    public final static short EVENT_SUSPEND = 10;
+    public final static short EVENT_OK = 0;
+    public final static short EVENT_KO = 2;
+    public final static short EVENT_INEXIST = 3;
+    public final static short EVENT_INEXIST_WORKER = 4;
+    public final static short ENGINE_KO = 200;
+	
+	void add(short event, db_batch batch, i_batch_log log);
+	void add(short event, String idBatch, i_batch_log log); 
+}
